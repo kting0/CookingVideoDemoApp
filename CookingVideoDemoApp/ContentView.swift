eea6eb3d4e822eb048a156f68e1487dfb7330551
Kuan-Ting Lin <contact@ting.tw>
2025-12-03 13:59:24 +0800

    Initial Commit

---
+++ CookingVideoDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookingVideoDemoApp
+//
+//  Created by Kuan-Ting Lin on 2025/12/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookingVideoDemoApp/CookingVideoDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingVideoDemoAppApp.swift
+//  CookingVideoDemoApp
+//
+//  Created by Kuan-Ting Lin on 2025/12/3.
+//
+
+import SwiftUI
+
+@main
+struct CookingVideoDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
